ebd467ea1d659a8f2b97d340735ef405255167fb
CHAN KAI CHENG <106880309+JeterChan@users.noreply.github.com>
2023-05-08 20:19:28 +0800

    Add files via upload

---
+++ department.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+    <head>
+        <title>醫二三</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">        <!--到mobile裝置上仍可正常顯示,指定螢幕寬度為裝置寬度,畫面載入初始縮放比例 100% -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <link rel="stylesheet" href="/page_css/department.css">
+    </head>
+
+    </body>
+        <!-- Navbar -->
+        <div class="container-fluid">
+            <nav class="navbar fixed-top navbar-dark" style="background-color: #2F4F4F;">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php">醫二三</a>
+                </div>
+            </nav>
+        </div>
+
+        <div class="content-box">
+            <h2>請選擇您要就診的科別</h2>
+        </div>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <button type="button" class="btn btn-outline-primary">家庭醫學科</button>
+                    <button type="button" class="btn btn-outline-primary">內科</button>
+                    <button type="button" class="btn btn-outline-primary">外科</button>
+                    <button type="button" class="btn btn-outline-primary">兒科</button>
+                    <button type="button" class="btn btn-outline-primary">婦產科</button>
+                </div>  
+            </div>
+
+            <div class="row">
+                <div class="col-sm">
+                    <button type="button" class="btn btn-outline-primary">骨科</button>
+                    <button type="button" class="btn btn-outline-primary">神經科</button>
+                    <button type="button" class="btn btn-outline-primary">神經外科</button>
+                    <button type="button" class="btn btn-outline-primary">泌尿科</button>
+                    <button type="button" class="btn btn-outline-primary">耳鼻喉科</button>
+                </div>  
+            </div>
+
+            <div class="row">
+                <div class="col-sm">
+                    <button type="button" class="btn btn-outline-primary">眼科</button>
+                    <button type="button" class="btn btn-outline-primary">皮膚科</button>
+                    <button type="button" class="btn btn-outline-primary">精神科</button>
+                    <button type="button" class="btn btn-outline-primary">復健科</button>
+                    <button type="button" class="btn btn-outline-primary">麻醉科</button>
+                </div>  
+            </div>
+
+            <div class="row">
+                <div class="col-sm">
+                    <button type="button" class="btn btn-outline-primary">放射診斷科</button>
+                    <button type="button" class="btn btn-outline-primary">放射腫瘤科</button>
+                    <button type="button" class="btn btn-outline-primary">解剖病理科</button>
+                    <button type="button" class="btn btn-outline-primary">臨床病理科</button>
+                    <button type="button" class="btn btn-outline-primary">整形外科</button>
+                </div>  
+            </div>
+
+            <div class="row">
+                <div class="col-sm">
+                    <button type="button" class="btn btn-outline-primary">急診醫學科</button>
+                    <button type="button" class="btn btn-outline-primary">職業醫學科</button>
+                    <button type="button" class="btn btn-outline-primary">西醫一般科</button>
+                    <button type="button" class="btn btn-outline-primary">牙醫一般科</button>
+                    <button type="button" class="btn btn-outline-primary">中醫一般科</button>
+                </div>  
+            </div>
+
+        </div>
+        
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    </body>
+</html>
