a4701f0563e301cad2d6826faca3606625516d2a
TATTThomas <92949208+TATTThomas@users.noreply.github.com>
2023-05-08 20:29:29 +0800

    Add files via upload

---
+++ choose_ill.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+<title>醫二三</title>
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">        <!--到mobile裝置上仍可正常顯示,指定螢幕寬度為裝置寬度,畫面載入初始縮放比例 100% -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/navbar.css">
+<link rel="stylesheet" href="/css/card.css">
+<link rel="stylesheet" href="/css/row.css">
+<link rel="stylesheet" href="/css/button.css">
+
+</head>
+<body>
+<div class="row">
+
+<div class="container-fluid">
+    <nav class="navbar fixed-top navbar-dark" style="background-color: #2F4F4F;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">醫二三</a>
+    </div>
+    </nav>
+</div>
+</div>
+
+<div class="row">
+<div class="container">
+<div class="row">
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>心臟不適</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>胸痛胸悶</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>感冒症狀</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>持續高燒</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>頭痛</span></button></div>
+</div>
+
+<div class="row">
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>頭暈</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>嘔吐</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>腹部不適</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>眼部症狀</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>耳部症狀</span></button></div>
+</div>
+
+<div class="row">
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>呼吸困難</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>關節疼痛</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>皮膚病症</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>手腳麻木</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>腰背痠痛</span></button></div>
+</div>
+
+<div class="row">
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>持續咳嗽</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>身體調理</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>兒童症狀</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>泌尿症狀</span></button></div>
+  <div class="col-sm"><button type="button" class="btn btn-primary btn-lg"><span>疼痛控制</span></button></div>
+</div>
+
+</div>
+</div>
