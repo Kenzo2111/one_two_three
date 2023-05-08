f0743bd2fcd1a296fe54203fff4d2798131bf105
Kenzo2111 <113497726+Kenzo2111@users.noreply.github.com>
2023-05-08 20:37:28 +0800

    Update quick_doc.php

---
+++ quick_doc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+<title>醫二三</title>
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">       
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/navbar.css">
+<link rel="stylesheet" href="/css/card.css">
+
+</head>
+<body>
+
+<div class="container-fluid">Always 100% wide
+    <nav class="navbar fixed-top navbar-dark" style="background-color: #2F4F4F;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">醫二三</a>
+    </div>
+    </nav>
+</div>
+<br><br>
+<div class="container">
+<h4 class="card-title">想好要看什麼科別了嗎？</h4><br>
+<button type="button" class="btn btn-outline-primary">想好了，點我找醫院</button>
+<button type="button" class="btn btn-outline-primary">還沒，點我查看可能需要的科別</button>
+</div>
